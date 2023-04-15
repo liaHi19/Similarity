e588bcf83157f308ec9b71fc55f58e109a10ece8
Nataliia Hirniak <gn.natalia19@gmail.com>
2023-04-15 20:50:59 +0100

    [#23] Add similarity api functionality

---
+++ src/pages/api/v1/similarity.ts
@@ -0,0 +1,68 @@
+import { openai } from "@/lib/open-ai";
+import { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
+
+import { cosineSimilarity } from "@/helpers/cosine-similarity";
+import { withMethods } from "@/lib/api-middlewares/with-methods";
+import { db } from "@/lib/db";
+
+const reqSchema = z.object({
+  text1: z.string().max(1000),
+  text2: z.string().max(1000),
+});
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const body = req.body as unknown;
+
+  const apiKey = req.headers.authorization;
+  if (!apiKey) {
+    return res.status(400).json({ error: "Unauthorized" });
+  }
+  try {
+    const { text1, text2 } = reqSchema.parse(body);
+
+    const validApiKey = await db.apiKey.findFirst({
+      where: { key: apiKey, enabled: true },
+    });
+
+    if (!validApiKey) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const start = new Date();
+
+    const embedding = await Promise.all(
+      [text1, text2].map(async (text) => {
+        const res = await openai.createEmbedding({
+          model: "text-embedding-ada-002",
+          input: text,
+        });
+
+        return res.data.data[0].embedding;
+      })
+    );
+
+    const similarity = cosineSimilarity(embedding[0], embedding[1]);
+
+    const duration = new Date().getTime() - start.getTime();
+
+    await db.apiRequest.create({
+      data: {
+        duration,
+        method: req.method as string,
+        path: req.url as string,
+        status: 200,
+        apiKeyId: validApiKey.id,
+        usedApiKey: validApiKey.key,
+      },
+    });
+    return res.status(200).json({ success: true, text1, text2, similarity });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({ error: error.issues });
+    }
+    return res.status(500).json({ error: "Internal Server Issue" });
+  }
+};
+
+export default withMethods(["POST"], handler);
